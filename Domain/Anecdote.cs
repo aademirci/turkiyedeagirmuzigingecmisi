59f32e8a684e8884ecd70f2952afafc873771ddf
A.A. Demirci <aalperdemirci@gmail.com>
2020-06-14 04:42:54 +0300

    Initial setting

---
+++ Domain/Anecdote.cs
@@ -0,0 +1,9 @@
+namespace Domain
+{
+    public class Anecdote
+    {
+        public int AnecdoteID { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+    }
+}
